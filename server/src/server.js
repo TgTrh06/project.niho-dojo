a9821ea1dc8cecece4abd8b6c14cd75fb2dfd063
Itsuki06 <trinhtung0604@gmail.com>
2025-10-03 11:09:39 +0700

    3 - Create auth routes/controllers

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import authRoutes from './routes/auth.route.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/main', (req, res) => {
+app.use(express.json());
+
+app.get('/test', (req, res) => {
     res.send('Hello, World!');
 });
+
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
